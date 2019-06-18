15dfd5649a43714c67095a470714624e567e303c
amclaug7 <44976695+amclaug7@users.noreply.github.com>
2019-06-18 14:50:58 -0400

    Add files via upload

---
+++ Roll Em/Roll Em/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Roll_Em
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRollDice = new System.Windows.Forms.Button();
+            this.pbxDie1 = new System.Windows.Forms.PictureBox();
+            this.pbxDie2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDie1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDie2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRollDice
+            // 
+            this.btnRollDice.Location = new System.Drawing.Point(13, 119);
+            this.btnRollDice.Name = "btnRollDice";
+            this.btnRollDice.Size = new System.Drawing.Size(226, 23);
+            this.btnRollDice.TabIndex = 0;
+            this.btnRollDice.Text = "Roll Em";
+            this.btnRollDice.UseVisualStyleBackColor = true;
+            this.btnRollDice.Click += new System.EventHandler(this.btnRollDice_Click);
+            // 
+            // pbxDie1
+            // 
+            this.pbxDie1.Location = new System.Drawing.Point(13, 13);
+            this.pbxDie1.Name = "pbxDie1";
+            this.pbxDie1.Size = new System.Drawing.Size(100, 100);
+            this.pbxDie1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxDie1.TabIndex = 1;
+            this.pbxDie1.TabStop = false;
+            // 
+            // pbxDie2
+            // 
+            this.pbxDie2.Location = new System.Drawing.Point(139, 13);
+            this.pbxDie2.Name = "pbxDie2";
+            this.pbxDie2.Size = new System.Drawing.Size(100, 100);
+            this.pbxDie2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxDie2.TabIndex = 2;
+            this.pbxDie2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 158);
+            this.Controls.Add(this.pbxDie2);
+            this.Controls.Add(this.pbxDie1);
+            this.Controls.Add(this.btnRollDice);
+            this.Name = "Form1";
+            this.Text = "Crap Table";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDie1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDie2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRollDice;
+        private System.Windows.Forms.PictureBox pbxDie1;
+        private System.Windows.Forms.PictureBox pbxDie2;
+    }
+}
+
---
+++ Roll Em/Roll Em/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Roll_Em
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random randomNumbers = new Random();
+
+        private void btnRollDice_Click(object sender, EventArgs e)
+        {
+            int i = randomNumbers.Next(1, 7); // 1 to 6
+            int j = randomNumbers.Next(1, 7);
+
+            pbxDie1.Image = (Image) (Properties.Resources.ResourceManager.GetObject(
+                $"dice{i}"));
+            pbxDie2.Image = (Image) (Properties.Resources.ResourceManager.GetObject(
+                $"dice{j}"));
+        }
+    }
+}
